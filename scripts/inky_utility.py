924e16bc6feb233f7c68d7f68deda37709258952
justDabuK <kevinjust87@gmail.com>
2023-11-05 19:32:35 +0100

    feat: introduce rotate_and_extend

--- scripts/inky_utility.py
+++ scripts/inky_utility.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageStat
 from inky.auto import auto
 
 display = auto()
@@ -26,6 +26,24 @@ def rotate_and_crop_image(image_name, input_dir="", output_dir=""):
     image_name_split = image_name.split('.')
     output_path = output_dir + image_name_split[0] + '_cropped_rotated.' + image_name_split[1]
     smaller_rotated_image.save(output_path)
+    return output_path
+
+
+def rotate_and_extend_image(image_name, input_dir="", output_dir=""):
+    image_path = input_dir + image_name
+    original_image = Image.open(image_path)
+    rotated_image = original_image.rotate(270, expand=True)
+    median_color = ImageStat.Stat(original_image).median
+
+    background_image = Image.new(rotated_image.mode, INKY_SCREEN_RESOLUTION, tuple(median_color))
+    left_offset = int((INKY_SCREEN_RESOLUTION[0] - rotated_image.size[0]) / 2)
+    top_offset = int((INKY_SCREEN_RESOLUTION[1] - rotated_image.size[1]) / 2)
+    background_image.paste(rotated_image, (left_offset, top_offset))
+
+    image_name_split = image_name.split('.')
+    output_path = output_dir + image_name_split[0] + '_extended_rotated.' + image_name_split[1]
+    background_image.save(output_path)
+
     return output_path
 
 
