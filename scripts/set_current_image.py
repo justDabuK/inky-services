a695089a1c7855774f341a42b2fc376f39caa39d
justDaBuK <kevinjust87@gmail.com>
2024-02-25 17:04:57 +0100

    feat: clean the current directory before setting the new image

--- scripts/set_current_image.py
+++ scripts/set_current_image.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import shutil
 from images_in_dir import get_image_choice
@@ -11,12 +12,19 @@ if __name__ == "__main__":
     """.format(file=sys.argv[0]))
         sys.exit(1)
 
+    # setup directory paths
     picture_dir = sys.argv[1]
-    choice = get_image_choice(f"{picture_dir}/originals")
+    originals_dir = f"{picture_dir}/originals"
+    current_dir = f"{picture_dir}/current"
+
+    # clean current directory
+    shutil.rmtree(current_dir)
+    os.mkdir(current_dir)
+
+    # choose a new image and copy it into the current folder
+    choice = get_image_choice(originals_dir)
     file_ending = choice.split(".")[-1]
-
     print(f"going to set {choice} as the current image")
-
     shutil.copy(choice, f"{picture_dir}/current/current.{file_ending}")
 
 
