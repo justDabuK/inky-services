59309bdcfd2454bde40d11b226b8dab0ec560ed8
justDaBuK <kevinjust87@gmail.com>
2024-02-25 18:27:17 +0100

    feat: add get and show current image

---
+++ scripts/get_and_show_current_image.py
@@ -0,0 +1,21 @@
+import requests
+import shutil
+from PIL import Image
+from inky.auto import auto
+
+inky = auto()
+
+if __name__ == "__main__":
+    url = "http://http://192.168.178.30:8000//images/current/get/download"
+
+    response = requests.put(url, json=inky.resolution, stream=True)
+    file_ending = response.headers['content-disposition'].split(';')[1].split('.')[-1].replace('"', '')
+    current_image_path = f"~/current_image.{file_ending}"
+
+    with open(current_image_path, "wb") as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
+
+    current_image = Image.open(current_image_path)
+    inky.set_image(current_image)
+    inky.show()
